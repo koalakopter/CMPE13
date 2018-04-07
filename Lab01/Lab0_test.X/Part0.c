af9a8cee2038e3d9683b4e509a3826473cdfb568
Julian To <jcto@ucsc.edu>
2018-04-06 22:50:43 -0700

    Did some stuff, added code and such

---
+++ Lab01/Lab0_test.X/Part0.c
@@ -0,0 +1,17 @@
+/*
+ * File:   Part0.c
+ * Author: julia
+ *
+ * Created on April 5, 2018, 1:53 PM
+ */
+
+
+#include "xc.h"
+#include "BOARD.h"
+
+void main(void)
+{
+    BOARD_Init();
+    printf("Hello World\n");
+    while(1); 
+}
