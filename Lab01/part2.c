17609c9528b4551bad8286f90ee175275920af7c
Julian To <jcto@ucsc.edu>
2018-04-08 14:16:59 -0700

    Added Kelvin conversion and headers

--- Lab01/part2.c
+++ Lab01/part2.c
@@ -29,14 +29,25 @@ int main(void)
     fahr = lower;
 
     //column headers
-    printf("%5c %5c \n", 'F', 'C'); 
+    printf("%5c %5c \n", 'F', 'C');
     //Print out table
     while (fahr <= upper) {
         celsius = (5.0 / 9.0)*(fahr - 32.0);
         printf("%7.1f %04.0f \n", (double) fahr, (double) celsius);
         fahr = fahr + step;
     }
+    printf("\n"); //add a new line between conversion tables
 
+    //Kelvin to Fahrenheit
+    float kelvin = lower;
+    //column header
+    printf("%5c %6c \n", 'K', 'F');
+    
+    while (kelvin <= upper) {
+        fahr = ((kelvin - 273.15)*1.8) + 32.0;
+        printf("%3.3f %5f\n", (double) kelvin, (double) fahr);
+        kelvin = kelvin + step;
+    }
 
     /***************************************************************************************************
      * Your code goes in between this comment and the preceding one with asterisks.
