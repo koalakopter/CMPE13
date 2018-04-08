e250bdde0d1ae44086b561b7514a73e7049e90bc
Julian To <jcto@ucsc.edu>
2018-04-08 14:45:33 -0700

    Added SimpleMath.h

---
+++ Lab01/Lab1_Part3.X/SimpleMath.h
@@ -0,0 +1,6 @@
+// module defining implementations of AddTwoNumbers and IncrementByOne 
+#ifndef SIMPLEMATH_H
+#define SIMPLEMATH_H
+int AddTwoNumbers (int x, int y);
+int IncrementByOne (int x);
+#endif
