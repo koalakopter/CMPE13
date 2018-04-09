aacc2ee2c0abbc58c44eeb2d42bdfeaf02f318d7
Julian To <jcto@ucsc.edu>
2018-04-09 15:27:28 -0700

    Added the necessary functions

--- Lab01/Lab1_Part3.X/SimpleMath.c
+++ Lab01/Lab1_Part3.X/SimpleMath.c
@@ -1,1 +1,13 @@
 #include "SimpleMath.h"
+
+int AddTwoNumbers(int x, int y)
+{
+    int z = x + y;
+    return z;
+}
+
+int IncrementByOne(int x)
+{
+    int y = AddTwoNumbers(x, 1);
+    return y;
+} 
