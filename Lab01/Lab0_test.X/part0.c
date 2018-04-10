5ee93c8a5560f7c954af4f728ca9b7782512290b
Julian To <jcto@ucsc.edu>
2018-04-09 23:32:35 -0700

    Did Hello World on the UNO32

--- Lab01/Lab0_test.X/part0.c
+++ Lab01/Lab0_test.X/part0.c
@@ -8,10 +8,18 @@
 
 #include "xc.h"
 #include "BOARD.h"
-
+//added for part4
+/*
+#include "Ascii.h"
+#include "Oled.h"
+#include "OledDriver.h" 
+*/
 void main(void)
 {
     BOARD_Init();
+    OledInit();
+    OledDrawString("Hello \nWorld! \n");
+    OledUpdate();
     printf("Hello World\n");
     while(1); 
 }
