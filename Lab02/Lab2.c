15ce7e5fbca269359dca03d83f8f3f89ad00d7d3
Julian To <jcto@ucsc.edu>
2018-04-13 14:08:39 -0700

    Wrote a welcome message

---
+++ Lab02/Lab2.c
@@ -0,0 +1,99 @@
+// **** Include libraries here ****
+// Standard libraries
+#include <stdio.h>
+#include <math.h>
+
+//CMPE13 Support Library
+#include "BOARD.h"
+
+// Microchip libraries
+#include <xc.h>
+#include <plib.h>
+
+// User libraries
+
+
+// **** Set macros and preprocessor directives ****
+
+// **** Define global, module-level, or external variables here ****
+
+// **** Declare function prototypes ****
+/******************************************************************************
+ * Your function prototypes go in between this comment and the following one with asterisks.
+ *****************************************************************************/
+double Round(double operand);
+
+
+
+
+/******************************************************************************
+ * Your function prototypes go in between this comment and the preceding one with asterisks
+ *****************************************************************************/
+
+
+// If this code is used for testing, rename main to something we can call from our testing code.
+#ifndef LAB2_TESTING
+
+int main(void)
+{
+    BOARD_Init();
+#else
+
+int their_main(void)
+{
+#endif // LAB2_TESTING
+
+    /******************************************************************************
+     * Your code goes in between this comment and the following one with asterisks.
+     *****************************************************************************/
+    printf("Welcome to the calculator!\n");
+
+
+
+
+    /******************************************************************************
+     * Your code goes in between this comment and the preceding one with asterisks
+     *****************************************************************************/
+    while (1);
+}
+
+/********************************************************************************
+ * Define the Absolute Value function here.
+ ********************************************************************************/
+
+
+
+/*********************************************************************************
+ * Define the Fahrenheit to Celsius function here.
+ ********************************************************************************/
+
+
+
+/*********************************************************************************
+ * Define the Celsius to Fahrenheit function here.
+ ********************************************************************************/
+
+
+
+/********************************************************************************
+ * Define the Average function here.
+ *******************************************************************************/
+
+
+
+/*********************************************************************************
+ * Define the Tangent function that takes input in degrees (Note: Your tangent 
+ * function relies on a call from the tangent function of math.h which uses 
+ * radians).
+ ********************************************************************************/
+
+/*********************************************************************************
+ * Define the Round function here.
+ * In order to receive the extra credit your calculator MUST ALSO CORRECTLY utilize
+ * this function.
+ ********************************************************************************/
+double Round(double operand)
+{
+    return operand;
+}
+
