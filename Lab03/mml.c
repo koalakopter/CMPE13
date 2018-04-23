e9968e48ec20c268745627153d676404b75cce36
Julian To <jcto@ucsc.edu>
2018-04-23 15:53:51 -0700

    Added Test Harness for MatrixEquals and MatrixScalarAdd

--- Lab03/mml.c
+++ Lab03/mml.c
@@ -20,6 +20,15 @@
 // **** Define global, module-level, or external variables here ****
 
 // **** Declare function prototypes ****
+//checks if a function passes a test, and if it does, increment a counter
+
+int pass(int check, int counter)
+{
+    if (check == 1) {
+        return counter + 1;
+    }
+    return counter;
+}
 
 int main()
 {
@@ -32,51 +41,69 @@ int main()
     float one[3][3] = {
         {1, 2, 3},
         {4, 5, 6},
-        {7, 8, 9}};
+        {7, 8, 9}
+    };
     float two[3][3] = {
         {1, 2, 3},
         {4, 5, 6},
-        {7, 8, 9}};
+        {7, 8, 9}
+    };
     float three[3][3];
-    float four[3][3];
-     float five[3][3] = {
+    float four[3][3] = {
         {5, 2, 1},
         {4, 5, 6},
-        {7, 4, 3}};
-    
-    /*TESTING SECTION BEWARE OF BUGS*/
-    int z = MatrixEquals(one, two);
-    printf("result is %d, should be one", z);
-    
-    
-    //MatrixMultiply(one, two, three);
-    //MatrixPrint(three);
-   
-    int scalar = 5;
-    MatrixScalarMultiply(scalar, one, three);
-    MatrixPrint(three);
-    
-    MatrixScalarAdd(2, one, four);
-    MatrixPrint(four);
-    
-    //MatrixPrint(one);
-     
-    float det = MatrixDeterminant(five);
-    printf("determinant is  %f", det);
-    /*
-    MatrixPrint(three);
-    float meme = MatrixTrace(three);
-    printf("trace is %f", meme);
-    
-    MatrixPrint(three);
-    MatrixTranspose(three, three);
-    MatrixPrint(three);*/
-    
+        {7, 4, 3}
+    };
 
-    MatrixPrint(five);
-    MatrixInverse(five, five);
-    MatrixPrint(five);
-   
+    /*TESTING HARNESS SECTION*/
+    //testing MatrixEquals
+    int count1 = 0; //counts how many tests passed
+    int testOne = MatrixEquals(one, two);
+    int testTwo = MatrixEquals(four, three);
+
+    if (testOne == 1) {
+        count1++;
+    }
+    if (testTwo == 0) {
+        count1++;
+    }
+    printf("MatrixEquals Function tested: %d out of 2 tests successful \n", count1);
+
+    //Testing MatrixScalarAdd
+    int count2 = 0;
+    float five[3][3] = {
+        {-1, -2, -3},
+        {-4, -5, -6},
+        {-7, -8, -9}
+    };
+    float six[3][3] = {
+        {-11, -12, -13},
+        {-14, -15, -16},
+        {-17, -18, -19}
+    };
+    float seven[3][3];
+    float eight[3][3] = {
+        {-5, -5, -5},
+        {-5, -5, -5},
+        {-5, -5, -5}
+    };
+    MatrixScalarAdd(-10, five, five);
+    MatrixScalarAdd(-5, seven, seven);
+
+    int testThree = MatrixEquals(five, six);
+    int testFour = MatrixEquals(seven, eight);
+
+    if (testThree == 1) {
+        count2++;
+    }
+    if (testFour == 1) {
+        count2++;
+    }
+    printf("MatrixScalarAdd Function tested: %d out of 2 tests successful \n", count2);
+
+
+
+
 
     /******************************************************************************
      * Your code goes in between this comment and the preceding one with asterisks
