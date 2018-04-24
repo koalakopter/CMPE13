3735052597cd28e14f77b19fe9eb1cf0d5560f33
Julian To <jcto@ucsc.edu>
2018-04-23 17:33:47 -0700

    Halfway through Test Harnesses

--- Lab03/mml.c
+++ Lab03/mml.c
@@ -22,12 +22,11 @@
 // **** Declare function prototypes ****
 //checks if a function passes a test, and if it does, increment a counter
 
-int pass(int check, int counter)
+void Checker(int check, int counter)
 {
-    if (check == 1) {
-        return counter + 1;
+    if (check != 1.0) {
+        counter++;
     }
-    return counter;
 }
 
 int main()
@@ -57,6 +56,7 @@ int main()
 
     /*TESTING HARNESS SECTION*/
     //testing MatrixEquals
+
     int count1 = 0; //counts how many tests passed
     int testOne = MatrixEquals(one, two);
     int testTwo = MatrixEquals(four, three);
@@ -101,7 +101,113 @@ int main()
     }
     printf("MatrixScalarAdd Function tested: %d out of 2 tests successful \n", count2);
 
+    //test for MatrixScalarMultiply
+    int count3 = 0;
+    float nine[3][3] = {
+        {-2, 5, -2},
+        {4, 2, 0},
+        {-1, -3, 6}
+    };
+    float ten[3][3] = {
+        {4, -10, 4},
+        {-8, -4, 0},
+        {2, 6, -12}
+    };
+    float eleven[3][3];
+    float twelve[3][3] = {
+        {0, 0, 0},
+        {0, 0, 0},
+        {0, 0, 0}
+    };
 
+    MatrixScalarMultiply(-2.0, nine, nine);
+    MatrixScalarMultiply(0, eleven, eleven);
+
+    int testFive = MatrixEquals(nine, ten);
+    int testSix = MatrixEquals(twelve, eleven);
+    if (testFive == 1) {
+        count3++;
+    }
+    if (testSix == 1) {
+        count3++;
+    }
+    printf("MatrixScalarMultiply Function tested: %d out of 2 tests successful \n", count3);
+
+    //test for MatrixMultiply
+    int count4;
+    float thirteen[3][3] = {
+        {1, 2, 3},
+        {4, 6, -3},
+        {-9, -3, 6}
+    };
+    float fourteen[3][3] = {
+        {2, 2, 3},
+        {-4, 9, 1},
+        {-8, 5, -2}
+    };
+    float fifteen[3][3] = {
+        {-30, 35, -1},
+        {8, 47, 24},
+        {-54, -15, -42}
+    };
+    float sixteen[3][3] = {
+        {1, 2, 3},
+        {9, 8, 7},
+        {4, 5, 6}
+    };
+    float seventeen[3][3] = {
+        {31, 33, 35},
+        {109, 117, 125},
+        {73, 78, 83}
+    };
+    float eighteen[3][3];
+    float nineteen[3][3];
+
+    MatrixMultiply(thirteen, fourteen, eighteen);
+    MatrixMultiply(sixteen, sixteen, nineteen);
+
+    int testSeven = MatrixEquals(eighteen, fifteen);
+    int testEight = MatrixEquals(nineteen, seventeen);
+    if (testSeven == 1) {
+        count4++;
+    }
+    if (testEight == 1) {
+        count4++;
+    }
+    printf("MatrixMultiply Function tested: %d out of 2 tests successful \n", count4);
+
+    //testing MatrixAdd
+    int count5;
+    float twenty[3][3] = {
+        {-1, -2, -3},
+        {-4, -5, -6},
+        {-7, -8, -9}
+    };
+    float twentyOne[3][3] = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+    float twentyTwo[3][3] = {
+        {0.5, 1, 1.5},
+        {2, 3, -1.5},
+        {-4.5, -1.5, 3}
+    };
+    float twentyThree[3][3];
+    float twentyFour[3][3];
+    
+    MatrixAdd(twenty, twentyOne, twentyThree);
+    MatrixAdd(twentyTwo, twentyTwo, twentyFour);
+    
+    int testNine = MatrixEquals(twentyThree, twelve);
+    int testTen = MatrixEquals(twentyFour, thirteen);
+    if (testNine == 1) {
+        count5++;
+    }
+    if (testTen == 1) {
+        count5++;
+    }
+    printf("MatrixAdd Function tested: %d out of 2 tests successful \n", count5);
 
 
 
