8d373946727b09d1759a85adbd3653eb0c1fdc07
Julian To <jcto@ucsc.edu>
2018-04-30 13:45:37 -0700

    StackInit and StackPush done

--- Lab04/Lab4.X/Stack.c
+++ Lab04/Lab4.X/Stack.c
@@ -1,4 +1,21 @@
 #include <stdio.h>
 #include "Stack.h"
 
-void StackInit(struct Stack *stack);
+void StackInit(struct Stack *stack)
+{
+    stack -> currentItemIndex = -1;
+    stack -> initialized = TRUE;
+}
+
+int StackPush(struct Stack *stack, float value)
+{
+    if (stack->initialized != TRUE) {
+        return STANDARD_ERROR;
+    }
+    stack->currentItemIndex += 1;
+    stack->stackItems[stack->currentItemIndex] = value; 
+    return SUCCESS;
+}
+
+
+
