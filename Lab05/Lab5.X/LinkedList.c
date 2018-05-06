5a286fe0ab9a71af1a465c1f2cbee4043304c291
Julian To <jcto@ucsc.edu>
2018-05-06 15:59:48 -0700

    Had to set a heap size???

--- Lab05/Lab5.X/LinkedList.c
+++ Lab05/Lab5.X/LinkedList.c
@@ -1,2 +1,16 @@
 #include <stdio.h>
 #include "LinkedList.h"
+
+ListItem *LinkedListNew(char *data)
+{
+    //sets up a pointer for a new list item
+    ListItem *currentList;
+    //if malloc fails, return null
+    if (currentList = (ListItem *) malloc(sizeof (ListItem))) {
+        currentList->data = data;
+        currentList->nextItem = NULL;
+        currentList->previousItem = NULL;
+        return currentList;
+    }
+    return NULL;
+}
