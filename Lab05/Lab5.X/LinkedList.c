48237759e0b4d0f5853113d8f11fed099821fa7d
Julian To <jcto@ucsc.edu>
2018-05-06 16:51:46 -0700

    Made the first three functions

--- Lab05/Lab5.X/LinkedList.c
+++ Lab05/Lab5.X/LinkedList.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h> 
 #include "LinkedList.h"
+
+//sets up a new linked list item
 
 ListItem *LinkedListNew(char *data)
 {
@@ -13,4 +16,42 @@ ListItem *LinkedListNew(char *data)
         return currentList;
     }
     return NULL;
+}
+
+//compare two Strings
+
+int CompareStrings(ListItem *first, ListItem *second)
+{
+    int x, y;
+    //if the data is not null, set the length of that string to x
+    if ((first->data) != NULL) {
+        x = strcmp(first->data);
+    }
+    //do the same thing for the second input
+    if ((second->data) != NULL) {
+        y = strcmp(second->data);
+    }
+
+    //compare the lengths 
+    //case 1, first str is alphabetically before second str
+    if (x < y) {
+        return -1;
+    }        //case 2, second str is alphabetically before first str
+    else if (x > y) {
+        return 1;
+    }        //case 3, they are equal
+    else {
+        return 0;
+    }
+
+}
+
+//Linked List Remove function
+char *LinkedListRemove(ListItem *item)
+{
+    //can't free after a return, so make a temp storage
+    ListItem *temp = LinkedListNew(NULL);
+    temp->data = item->data;
+    free(item);
+    return temp->data;
 }
