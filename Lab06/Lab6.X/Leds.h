9123e12d87619952349d11639c94cc829b850da0
Julian To <jcto@ucsc.edu>
2018-05-12 17:49:56 -0700

    Maybe it works?

--- Lab06/Lab6.X/Leds.h
+++ Lab06/Lab6.X/Leds.h
@@ -1,6 +1,18 @@
 //LAB 6 PART 1 bounce_switch.c
 //created by Julian To
+#ifndef LEDS_H
+#define LEDS_H
+
 #include "BOARD.h"
 #include <xc.h>
 
-#define LEDS_INIT()
+#define LEDS_INIT() do {
+TRISE = 0xF6; \//1111 0110 
+LATE = 0x09; \//0000 1001
+} while (0)
+    
+#define LEDS_GATE() 
+
+#define LEDS_SET(x)
+
+#endif //end definition
