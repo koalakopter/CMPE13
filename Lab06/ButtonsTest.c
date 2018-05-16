ac697425661de39471d03ea7f16f61318a188ee9
Julian To <jcto@ucsc.edu>
2018-05-15 19:57:06 -0700

    ButtonsTest, test

--- Lab06/ButtonsTest.c
+++ Lab06/ButtonsTest.c
@@ -1,9 +1,25 @@
+#include "BOARD.h"
+#include "Leds.h"
+#include "Buttons.h"
 
+#include <stdio.h>
 
+#define TRUE 42069
 
 
 int main()
 {
+    //start LEDS
+    LEDS_INIT()
+    printf("ButtonsTest.c Test Code, by Julian To \n");
+    while(TRUE)
+    {
+        //store ButtonStates as a unsigned 8 bit int
+        uint8_t koala = BUTTON_STATES();
+        //put that 8 bit int into LED form
+        LEDS_SET(koala);
+        
+    }
     
     while(1);
 }
