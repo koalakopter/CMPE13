6e006b159881b609c2dc395126c3f27659b051dc
Julian To <jcto@ucsc.edu>
2018-05-15 21:24:39 -0700

    SEND HELPPP

--- Lab06/ButtonsTest.c
+++ Lab06/ButtonsTest.c
@@ -6,21 +6,53 @@
 
 #define TRUE 42069
 
-
-int main()
+int main(void)
 {
     //start LEDS
     LEDS_INIT()
+
+    //test Buttons_Init()
+    ButtonsInit();
     printf("ButtonsTest.c Test Code, by Julian To \n");
-    while(TRUE)
-    {
+    while (TRUE) {
+        /*
         //store ButtonStates as a unsigned 8 bit int
         uint8_t koala = BUTTON_STATES();
         //put that 8 bit int into LED form
         LEDS_SET(koala);
         //REPORT: pressing buttons made LEDS light up WOWZERS
-        
+         */
+        uint8_t buttonEvents = ButtonsCheckEvents();
+        //copied from the lab manual
+        if (buttonEvents) {
+            if (buttonEvents & BUTTON_EVENT_1UP) {
+                puts("BUTTON_EVENT_1UP\n");
+            }
+            if (buttonEvents & BUTTON_EVENT_1DOWN) {
+                puts("BUTTON_EVENT_1DOWN\n");
+            }
+            if (buttonEvents & BUTTON_EVENT_2UP) {
+                puts("BUTTON_EVENT_2UP\n");
+            }
+            if (buttonEvents & BUTTON_EVENT_2DOWN) {
+                puts("BUTTON_EVENT_2DOWN\n");
+            }
+            if (buttonEvents & BUTTON_EVENT_3UP) {
+                puts("BUTTON_EVENT_3UP\n");
+            }
+            if (buttonEvents & BUTTON_EVENT_3DOWN) {
+                puts("BUTTON_EVENT_3DOWN\n");
+            }
+            if (buttonEvents & BUTTON_EVENT_4UP) {
+                puts("BUTTON_EVENT_4UP\n");
+            }
+            if (buttonEvents & BUTTON_EVENT_4DOWN) {
+                puts("BUTTON_EVENT_4DOWN\n");
+            }
+            buttonEvents = BUTTON_EVENT_NONE;
+
+        }
+
+        while (1);
     }
-    
-    while(1);
 }
