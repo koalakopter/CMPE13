7193810eb708541b8a2abac5c52a479bda8ae9ec
Julian To <jcto@ucsc.edu>
2018-05-15 18:38:27 -0700

    Did ButtonsInit? maybe?

--- Lab06/Buttons.c
+++ Lab06/Buttons.c
@@ -2,3 +2,17 @@
 #include <xc.h>
 
 #include <stdio.h>
+
+//buttons init function
+void ButtonsInit(void)
+{
+    //copied from the lab manual
+    TRISD |= 0x00E0;
+    TRISF |= 0x0002;
+}
+
+//ButtonsCheckEvents function
+uint8_t ButtonsCheckEvents(void)
+{
+    
+}
