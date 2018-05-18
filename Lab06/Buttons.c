471348bca1313578bc392acb8c6c222717b7e151
Julian To <jcto@ucsc.edu>
2018-05-18 12:57:37 -0700

    Forgot to check for additional warnings... whoops

--- Lab06/Buttons.c
+++ Lab06/Buttons.c
@@ -6,6 +6,9 @@
 #define AND &
 #define OR |
 
+#define TRUE 1
+#define FALSE 0
+
 //buttons init function
 
 void ButtonsInit(void)
@@ -25,8 +28,9 @@ void ButtonsInit(void)
  */
 //ButtonsCheckEvents function
 
-//keeps track of the current bits that are toggled
-static uint8_t current; //static so it can't be accessed outside of file
+//keeps track of the current buttons that are down or up
+//also keeps value between function calls
+static int oneD, twoD, threeD, fourD;
 
 uint8_t ButtonsCheckEvents(void)
 {
@@ -35,34 +39,32 @@ uint8_t ButtonsCheckEvents(void)
      * the button is being pressed and 0 if it's not. The buttons are ordered such that bit 3 is button
      * 4 and bit 0 is button 1. */
     uint8_t state = BUTTON_STATES();
-    uint8_t result = 0; //used to store the end result and is returned at the end of the function.
+    uint8_t result = BUTTON_EVENT_NONE; //used to store the end result and is returned at the end of the function.
+
     //will be used for bitwise OR operations
+    static uint8_t upDownCheck; //static so it can't be accessed outside of file
 
-    current = current + 0; //I'm a stupid statement that makes errros go away
+    upDownCheck = upDownCheck + 0; //I'm a stupid statement that makes errors go away
 
-    //if nothing changed return nothing
-    if (state == current) {
-        return BUTTON_EVENT_NONE;
-    }
 
     //checks if current state and button_state_1 match
     if (state AND BUTTON_STATE_1) {
-        //if current does not have the bits for button_state_1 activated, turn those bits on
-        if (!(current AND BUTTON_STATE_1)) {
+        //check if the button has been pressed before without being released
+        if (oneD == FALSE) {
 
-            //toggle appropriate bits
-            current = current OR BUTTON_STATE_1;
+            //recognize that the button has now been pressed
+            oneD = TRUE;
             //result now holds and stores button 1's down action
             result = result OR BUTTON_EVENT_1DOWN;
         }
-    }        //check if state and the button_state_1 don't match
+    }//check if state and the button_state_1 don't match
     else if (!(state AND BUTTON_STATE_1)) {
 
-        //if they current and state1 match, that means button was released
-        if (current AND BUTTON_STATE_1) {
+        //check if the button has been pressed before without being released
+        if (oneD == TRUE) {
 
-            //invert the bits of state1 then AND it with current
-            current = current AND (~BUTTON_STATE_1);
+            //recognize that the button has now been released
+            oneD = FALSE;
             //change result to store the up state
             result = result OR BUTTON_EVENT_1UP;
         }
@@ -70,69 +72,70 @@ uint8_t ButtonsCheckEvents(void)
     //its just copy paste from here on out
     //BUTTON 2 CODE
     if (state AND BUTTON_STATE_2) {
-        //if current does not have the bits for button_state_2 activated, turn those bits on
-        if (!(current AND BUTTON_STATE_2)) {
+        //check if the button has been pressed before without being released
+        if (twoD == FALSE) {
 
-            //toggle appropriate bits
-            current = current OR BUTTON_STATE_2;
+            //recognize that the button has now been pressed
+            twoD = TRUE;
             //result now holds and stores button 2's down action
             result = result OR BUTTON_EVENT_2DOWN;
         }
-    }        //check if state and the button_state_2 don't match
+    }//check if state and the button_state_2 don't match
     else if (!(state AND BUTTON_STATE_2)) {
 
-        //if they current and state2 match, that means button was released
-        if (current AND BUTTON_STATE_2) {
+       //check if the button has been pressed before without being released
+        if (twoD == TRUE) {
 
-            //invert the bits of state2 then AND it with current
-            current = current AND (~BUTTON_STATE_2);
+            //recognize that the button has now been released
+            twoD = FALSE;
             //change result to store the up state
             result = result OR BUTTON_EVENT_2UP;
         }
     }
     //BUTTON 3 CODE
     if (state AND BUTTON_STATE_3) {
-        //if current does not have the bits for button_state_3 activated, turn those bits on
-        if (!(current AND BUTTON_STATE_3)) {
+        //check if the button has been pressed before without being released
+        if (threeD == FALSE) {
 
-            //toggle appropriate bits
-            current = current OR BUTTON_STATE_3;
+            //recognize that the button has now been pressed
+            threeD = TRUE;
             //result now holds and stores button 3's down action
             result = result OR BUTTON_EVENT_3DOWN;
         }
-    }        //check if state and the button_state_3 don't match
+    }//check if state and the button_state_3 don't match
     else if (!(state AND BUTTON_STATE_3)) {
 
-        //if they current and state3 match, that means button was released
-        if (current AND BUTTON_STATE_3) {
+        //check if the button has been pressed before without being released
+        if (threeD == TRUE) {
 
-            //invert the bits of state3 then AND it with current
-            current = current AND (~BUTTON_STATE_3);
+            //recognize that the button has now been released
+            threeD = FALSE;
             //change result to store the up state
             result = result OR BUTTON_EVENT_3UP;
         }
     }
     //BUTTON 4 CODE
     if (state AND BUTTON_STATE_4) {
-        //if current does not have the bits for button_state_4 activated, turn those bits on
-        if (!(current AND BUTTON_STATE_4)) {
+        //check if the button has been pressed before without being released
+        if (fourD == FALSE) {
 
-            //toggle appropriate bits
-            current = current OR BUTTON_STATE_4;
+            //recognize that the button has now been pressed
+            fourD = TRUE;
             //result now holds and stores button 4's down action
             result = result OR BUTTON_EVENT_4DOWN;
         }
-    }        //check if state and the button_state_4 don't match
+    }//check if state and the button_state_4 don't match
     else if (!(state AND BUTTON_STATE_4)) {
 
-        //if they current and state4 match, that means button was released
-        if (current AND BUTTON_STATE_4) {
+        //check if the button has been pressed before without being released
+        if (fourD == TRUE) {
 
-            //invert the bits of state4 then AND it with current
-            current = current AND (~BUTTON_STATE_4);
+            //recognize that the button has now been released
+            fourD = FALSE;
             //change result to store the up state
             result = result OR BUTTON_EVENT_4UP;
         }
     }
+    //will return BUTTON_EVENT_NONE if nothing changed
     return result;
 }
