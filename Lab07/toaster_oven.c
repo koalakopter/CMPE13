fa10655a4dc3db6a6f661be215e78fb55f32742f
Julian To <jcto@ucsc.edu>
2018-05-20 15:51:06 -0700

    Started the lab!

---
+++ Lab07/toaster_oven.c
@@ -0,0 +1,100 @@
+// **** Include libraries here ****
+// Standard libraries
+
+//CMPE13 Support Library
+#include "BOARD.h"
+#include "Adc.h"
+#include "Ascii.h"
+#include "Buttons.h"
+#include "Leds.h"
+#include "Oled.h"
+#include "OledDriver.h"
+
+// Microchip libraries
+#include <xc.h>
+#include <plib.h>
+#include <stdio.h>
+
+
+
+// **** Set any macros or preprocessor directives here ****
+// Set a macro for resetting the timer, makes the code a little clearer.
+#define TIMER_2HZ_RESET() (TMR1 = 0)
+
+// **** Declare any datatypes here ****
+
+// **** Define any module-level, global, or external variables here ****
+
+// Configuration Bit settings
+
+int main()
+{
+    BOARD_Init();
+
+    // Configure Timer 1 using PBCLK as input. We configure it using a 1:256 prescalar, so each timer
+    // tick is actually at F_PB / 256 Hz, so setting PR1 to F_PB / 256 / 2 yields a 0.5s timer.
+    OpenTimer1(T1_ON | T1_SOURCE_INT | T1_PS_1_256, BOARD_GetPBClock() / 256 / 2);
+
+    // Set up the timer interrupt with a medium priority of 4.
+    INTClearFlag(INT_T1);
+    INTSetVectorPriority(INT_TIMER_1_VECTOR, INT_PRIORITY_LEVEL_4);
+    INTSetVectorSubPriority(INT_TIMER_1_VECTOR, INT_SUB_PRIORITY_LEVEL_0);
+    INTEnable(INT_T1, INT_ENABLED);
+
+    // Configure Timer 2 using PBCLK as input. We configure it using a 1:16 prescalar, so each timer
+    // tick is actually at F_PB / 16 Hz, so setting PR2 to F_PB / 16 / 100 yields a .01s timer.
+    OpenTimer2(T2_ON | T2_SOURCE_INT | T2_PS_1_16, BOARD_GetPBClock() / 16 / 100);
+
+    // Set up the timer interrupt with a medium priority of 4.
+    INTClearFlag(INT_T2);
+    INTSetVectorPriority(INT_TIMER_2_VECTOR, INT_PRIORITY_LEVEL_4);
+    INTSetVectorSubPriority(INT_TIMER_2_VECTOR, INT_SUB_PRIORITY_LEVEL_0);
+    INTEnable(INT_T2, INT_ENABLED);
+
+    // Configure Timer 3 using PBCLK as input. We configure it using a 1:256 prescalar, so each timer
+    // tick is actually at F_PB / 256 Hz, so setting PR3 to F_PB / 256 / 5 yields a .2s timer.
+    OpenTimer3(T3_ON | T3_SOURCE_INT | T3_PS_1_256, BOARD_GetPBClock() / 256 / 5);
+
+    // Set up the timer interrupt with a medium priority of 4.
+    INTClearFlag(INT_T3);
+    INTSetVectorPriority(INT_TIMER_3_VECTOR, INT_PRIORITY_LEVEL_4);
+    INTSetVectorSubPriority(INT_TIMER_3_VECTOR, INT_SUB_PRIORITY_LEVEL_0);
+    INTEnable(INT_T3, INT_ENABLED);
+
+    /***************************************************************************************************
+     * Your code goes in between this comment and the following one with asterisks.
+     **************************************************************************************************/
+    LEDS_INIT();
+    OledInit();
+    ButtonsInit();
+    AdcInit();
+    
+    
+
+
+    /***************************************************************************************************
+     * Your code goes in between this comment and the preceding one with asterisks
+     **************************************************************************************************/
+    while (1);
+}
+
+void __ISR(_TIMER_1_VECTOR, ipl4auto) TimerInterrupt2Hz(void)
+{
+    // Clear the interrupt flag.
+    IFS0CLR = 1 << 4;
+
+}
+
+void __ISR(_TIMER_3_VECTOR, ipl4auto) TimerInterrupt5Hz(void)
+{
+    // Clear the interrupt flag.
+    IFS0CLR = 1 << 12;
+
+}
+
+void __ISR(_TIMER_2_VECTOR, ipl4auto) TimerInterrupt100Hz(void)
+{
+    // Clear the interrupt flag.
+    IFS0CLR = 1 << 8;
+
+}
