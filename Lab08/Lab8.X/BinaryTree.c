d0a48e92a2796f3decc83ac57d0aada5eafe84d8
Julian To <jcto@ucsc.edu>
2018-05-28 00:12:12 -0700

    tree create did done

--- Lab08/Lab8.X/BinaryTree.c
+++ Lab08/Lab8.X/BinaryTree.c
@@ -1,8 +1,42 @@
 //BinaryTree.C
 #include "BinaryTree.h"
+#include "Board.h"
+#include <stdio.h>
 
 
-Node *TreeCreate(int level, const char *data)
-{
-    
+//create a tree
+int x; //keeps track of the array element
+
+Node *TreeCreate(int level, const char *data) {
+    //first, allocate space
+    Node *new = malloc(sizeof (Node));
+    //if the malloc fails, return null
+    if (new == NULL) {
+        return NULL;
+    }        //else, store the variable
+    else {
+        new->data = data[x];
+    }
+    x++; //move up the array;
+    //if not level 1, create a left and right child
+    if(level > 1)
+    {
+        level--;
+        new->leftChild = TreeCreate(level, data);
+        new->rightChild = TreeCreate(level, data);
+        level++;
+    }
+    //if at the top level, no left or right child
+    else if(level == 1)
+    {
+        new->leftChild = NULL;
+        new->rightChild = NULL;
+    }
+    return new;
+}
+
+//prints out a tree
+
+void PrintTree(Node *node, int space) {
+
 }
