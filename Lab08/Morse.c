76c4f5b3fa962157549f139d4eb28627046ab480
Julian To <jcto@ucsc.edu>
2018-05-28 21:50:22 -0700

    Morse Decode

--- Lab08/Morse.c
+++ Lab08/Morse.c
@@ -6,25 +6,55 @@
 #include <stdio.h>
 
 //array for the morse characters, each line is  a tree with 4 levels, E and T are the 5th level
-char morseChars[] = {NULL, 'E', 
-                    'I', 'S', 'H', '5', '4', 'V', NULL, '3', 'U', 'F', NULL, NULL, NULL, NULL, '2',
-                    'A', 'R', 'L', NULL, NULL, NULL, NULL, NULL, 'W', 'P', NULL, NULL, 'J', NULL, '1',
-                    'T', 
-                    'N', 'D', 'B', '6', NULL, 'X', NULL, NULL, 'K', 'C', NULL, NULL, 'Y', NULL, NULL,
-                    'M', 'G', 'Z', '7', NULL, 'Q', NULL, NULL, 'O', NULL, NULL, '8', NULL, '9', '0'};
+char morseChars[] = {NULL, 'E',
+    'I', 'S', 'H', '5', '4', 'V', NULL, '3', 'U', 'F', NULL, NULL, NULL, NULL, '2',
+    'A', 'R', 'L', NULL, NULL, NULL, NULL, NULL, 'W', 'P', NULL, NULL, 'J', NULL, '1',
+    'T',
+    'N', 'D', 'B', '6', NULL, 'X', NULL, NULL, 'K', 'C', NULL, NULL, 'Y', NULL, NULL,
+    'M', 'G', 'Z', '7', NULL, 'Q', NULL, NULL, 'O', NULL, NULL, '8', NULL, '9', '0'};
 
 int treeSize = 6;
+Node *tree;
+Node *temp;
+
 int MorseInit(void)
 {
     //initialize buttons
     ButtonsInit();
     //create new Morse tree based upon above array
     Node *tree = TreeCreate(treeSize, morseChars);
-    if(tree != NULL)
-    {
+    temp = tree; //will be used for decoding
+    if (tree != NULL) {
         return SUCCESS;
-    }
-    else {
+    } else {
         return STANDARD_ERROR;
     }
+}
+
+char MorseDecode(MorseChar in)
+{
+    //if not initialized, return NULL
+    if (tree == NULL) {
+        return STANDARD_ERROR;
+    }
+    if (in == MORSE_CHAR_DASH) {
+        //move to the right of the tree if dash
+        tree = tree->rightChild;
+        return SUCCESS;
+    } else if (in == MORSE_CHAR_DOT) {
+        //move left if dot
+        tree = tree->leftChild;
+        return SUCCESS;
+    } else if (in == MORSE_CHAR_END_OF_CHAR) {
+        return tree->data; // return data if end of input
+    } else if (in == MORSE_CHAR_DECODE_RESET) {
+        tree = temp; // set tree back to head of tree to read next char
+        return SUCCESS;
+    }
+    return in;
+}
+
+MorseEvent MorseCheckEvents(void)
+{
+    return 1;
 }
