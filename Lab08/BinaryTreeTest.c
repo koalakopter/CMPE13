640600a39327c6767d8dcd6fb90055eb062424e8
Julian To <jcto@ucsc.edu>
2018-05-29 17:53:24 -0700

    Writing the test harness meow

--- Lab08/BinaryTreeTest.c
+++ Lab08/BinaryTreeTest.c
@@ -13,6 +13,8 @@
 
 // User libraries
 #include "BinaryTree.h"
+#include "Morse.h"
+#include "Buttons.h"
 
 // **** Set any macros or preprocessor directives here ****
 
@@ -22,7 +24,6 @@
 
 // **** Declare any function prototypes here ****
 
-
 int main()
 {
     BOARD_Init();
@@ -30,18 +31,37 @@ int main()
     /******************************************************************************
      * Your code goes in between this comment and the following one with asterisks.
      *****************************************************************************/
-	 printf("\nTesting for Lab 8 and Julian's BinaryTree.C function");
-     
-     //creating some fun for the whole family
-     //data
-     char fletcher[] = {'a', 'b', 'd', 'e', 'c', 'f', 'g'};
-     int gearing = 3;
-     Node *test = TreeCreate(gearing, fletcher); 
-     PrintTree(test, 0);
+    printf("\nTesting for Lab 8 and Julian's BinaryTree.C function");
+
+    //creating some fun for the whole family
+    //data
+    char fletcher[] = {'a', 'b', 'd', 'e', 'c', 'f', 'g'};
+    printf("\nPRINT TEST 1: SHOULD PRINT THIS TREE BUT ROTATED CCW: \n");
+    printf("\n          a");
+    printf("\n    b           c");
+    printf("\nd       e     f       g");
+    int gearing = 3;
+
+    printf("\n\n\n"); //I've got a blank space, baby, and I'll write your name~
+
+    Node *test = TreeCreate(gearing, fletcher);
+    PrintTree(test, 0);
+
+    //test morse tree
+    char morseChars[] = {NULL, 'E', 
+                   'I', 'S', 'H', '5', '4', 'V', NULL, '3', 'U', 'F', NULL, NULL, NULL, NULL, '2',
+                   'A', 'R', 'L', NULL, NULL, NULL, NULL, NULL, 'W', 'P', NULL, NULL, 'J', NULL, '1',
+                   'T', 
+                   'N', 'D', 'B', '6', NULL, 'X', NULL, NULL, 'K', 'C', NULL, NULL, 'Y', NULL, NULL,
+                   'M', 'G', 'Z', '7', NULL, 'Q', NULL, NULL, 'O', NULL, NULL, '8', NULL, '9', '0'}; 
+    char benson[] = {'1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+    gearing = 4;
+    Node *morseTest = TreeCreate(gearing, benson);
+    PrintTree(morseTest, 0);
     /******************************************************************************
      * Your code goes in between this comment and the preceding one with asterisks
      *****************************************************************************/
 
-    while (1);                                                                  
+    while (1);
 }
 
