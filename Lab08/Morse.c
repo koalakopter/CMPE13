df14e9fe87d86f0e131c42c9e00b249362cc0b81
Julian To <jcto@ucsc.edu>
2018-05-30 16:53:32 -0700

    fixed MorseCheckEvents function... I think

--- Lab08/Morse.c
+++ Lab08/Morse.c
@@ -30,6 +30,7 @@ enum {
 };
 
 BOOLEAN isCreate;
+
 int MorseInit(void)
 {
     //initialize buttons and OLED
@@ -78,83 +79,74 @@ char MorseDecode(MorseChar in)
 uint8_t buttonEvent;
 int timer; //100 hz timer
 int currentEvent;
+
 MorseEvent MorseCheckEvents(void)
 {
     currentEvent = MORSE_EVENT_NONE;
     buttonEvent = ButtonsCheckEvents();
-    
+
     //if morse init hasnt been called, return an error
-    if(isCreate == FALSE)
-    {
+    if (isCreate == FALSE) {
         return STANDARD_ERROR;
     }
 
     switch (state) {
-    //case 1, waiting for input
+        //case 1, waiting for input
     case WAITING:
         //if button4 is pressed, reset and start the 100hz timer
-        if (buttonEvent & BUTTON_EVENT_4DOWN)
-        {
+        if (buttonEvent & BUTTON_EVENT_4DOWN) {
             //go to next case (DOT))
             state = DOT;
             timer = 0;
         }
         //if nothing happens, return nothing
         break;
-    //checks for DOT Press, or when the press is less than 0.5 sec but greater than 0.25 seconds
-    //note at 100hz, 25 triggers for 0.25 seconds
+        //checks for DOT Press, or when the press is less than 0.5 sec but greater than 0.25 seconds
+        //note at 100hz, 25 triggers for 0.25 seconds
     case DOT:
         //register a button being released
-        if (buttonEvent & BUTTON_EVENT_4UP)
-        {
+        if (buttonEvent & BUTTON_EVENT_4UP) {
             //check the time 
-            if (timer >= MORSE_EVENT_LENGTH_DOWN_DOT && 
-                     timer < MORSE_EVENT_LENGTH_DOWN_DASH)
-            {
+            if (timer >= MORSE_EVENT_LENGTH_DOWN_DOT &&
+                    timer < MORSE_EVENT_LENGTH_DOWN_DASH) {
                 //returns a dot
                 state = INTER_LETTER;
                 timer = 0; //reset the timer
                 currentEvent = MORSE_EVENT_DOT;
-            }
-            else if(timer >= MORSE_EVENT_LENGTH_DOWN_DASH)
-            {
+            } else if (timer >= MORSE_EVENT_LENGTH_DOWN_DASH) {
                 state = DASH;
                 break;
             }
         }
     case DASH:
-        
+
         timer = 0;
         state = INTER_LETTER;
         currentEvent = MORSE_EVENT_DASH; //returns a dash
         break;
-        
+
     case INTER_LETTER:
-        
         //if the value of timer is too big (>200), designate a new word
-        if (timer >= MORSE_EVENT_LENGTH_UP_INTER_LETTER_TIMEOUT)
-        {
+        if (timer > MORSE_EVENT_LENGTH_UP_INTER_LETTER_TIMEOUT) {
             state = WAITING;
             currentEvent = MORSE_EVENT_INTER_WORD;
             break;
         }
-        //check if button4 was pressed
-        if (buttonEvent & BUTTON_EVENT_4DOWN)
-        {
-            //if timer times out past 100 ticks, designate a new letter
-            if(timer >= MORSE_EVENT_LENGTH_UP_INTER_LETTER)
-            {
-                state = WAITING;
-                currentEvent =  MORSE_EVENT_INTER_LETTER;
-                break;
-            }
-            //if the interval was too short, its the same letter
-            else if(timer < MORSE_EVENT_LENGTH_UP_INTER_LETTER)
-            {
-                state = DOT;
-                timer = 0; //reset the timer and go back to dot
-            }
+        //if timer times out past 100 ticks, designate a new letter, reset timer
+        if (timer > MORSE_EVENT_LENGTH_UP_INTER_LETTER 
+                && (buttonEvent & BUTTON_EVENT_4DOWN)) {
+            state = DOT;
+            currentEvent = MORSE_EVENT_INTER_LETTER;
+            timer = 0;
+            break;
         }
+        //if the interval was too short and a button was pressed, its the same letter
+        else if ((timer < MORSE_EVENT_LENGTH_UP_INTER_LETTER) 
+                && (buttonEvent & BUTTON_EVENT_4DOWN)) {
+            state = DOT;
+            timer = 0; //reset the timer and go back to dot
+        }
+
         break;
     }
     timer++; //increment dat timer
