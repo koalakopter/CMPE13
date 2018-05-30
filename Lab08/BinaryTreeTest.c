8e0d9a45c81baed3a46f009846bbc410f3edac89
Julian To <jcto@ucsc.edu>
2018-05-29 18:39:51 -0700

    left and right test numba 1

--- Lab08/BinaryTreeTest.c
+++ Lab08/BinaryTreeTest.c
@@ -44,17 +44,31 @@ int main()
 
     printf("\n\n\n"); //I've got a blank space, baby, and I'll write your name~
 
-    Node *test = TreeCreate(gearing, fletcher);
+    Node *test = TreeCreate(gearing, fletcher); //create a tree
     PrintTree(test, 0);
 
+    printf("\n\n\n"); //I've got a blank space, baby, and I'll write your name~
+    
+    //testing for getLeft and getRight
+    Node *left;
+    Node *right;
+
+    left = GetLeftChild(test);
+    right = GetRightChild(test);
+    
+    printf("\nTESTING GetLeftChild and GetRightChild functions\n");
+    printf("got left child of first test tree: %c, expected: 'd'\n", left->data);
+    printf("got right child of first test tree: %c, expected: 'g'\n", right->data);
+
+
     //test morse tree
-    char morseChars[] = {NULL, 'E', 
-                   'I', 'S', 'H', '5', '4', 'V', NULL, '3', 'U', 'F', NULL, NULL, NULL, NULL, '2',
-                   'A', 'R', 'L', NULL, NULL, NULL, NULL, NULL, 'W', 'P', NULL, NULL, 'J', NULL, '1',
-                   'T', 
-                   'N', 'D', 'B', '6', NULL, 'X', NULL, NULL, 'K', 'C', NULL, NULL, 'Y', NULL, NULL,
-                   'M', 'G', 'Z', '7', NULL, 'Q', NULL, NULL, 'O', NULL, NULL, '8', NULL, '9', '0'}; 
-    char benson[] = {'1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+    char morseChars[] = {NULL, 'E',
+        'I', 'S', 'H', '5', '4', 'V', NULL, '3', 'U', 'F', NULL, NULL, NULL, NULL, '2',
+        'A', 'R', 'L', NULL, NULL, NULL, NULL, NULL, 'W', 'P', NULL, NULL, 'J', NULL, '1',
+        'T',
+        'N', 'D', 'B', '6', NULL, 'X', NULL, NULL, 'K', 'C', NULL, NULL, 'Y', NULL, NULL,
+        'M', 'G', 'Z', '7', NULL, 'Q', NULL, NULL, 'O', NULL, NULL, '8', NULL, '9', '0'};
+    char benson[] = {'1', '2', '3', '4', '5', '6', '7', '8', '9', NULL, 'B', 'C', 'D', 'E', 'F'};
     gearing = 4;
     Node *morseTest = TreeCreate(gearing, benson);
     PrintTree(morseTest, 0);
