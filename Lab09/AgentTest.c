2f78df8e1b5d28d57a4263ededbb616e7fd0ed76
Julian To <jcto@ucsc.edu>
2018-06-01 15:03:24 -0700

    Initial files

---
+++ Lab09/AgentTest.c
@@ -0,0 +1,41 @@
+// **** Include libraries here ****
+// Standard libraries
+
+//CMPE13 Support Library
+#include "BOARD.h"
+
+// Microchip libraries
+#include <xc.h>
+#include <plib.h>
+
+// User libraries
+
+
+// **** Set any macros or preprocessor directives here ****
+
+// **** Declare any data types here ****
+
+// **** Define any module-level, global, or external variables here ****
+
+
+// **** Declare any function prototypes here ****
+
+
+
+int main()
+{
+    BOARD_Init();
+
+
+
+/******************************************************************************
+ * Your code goes in between this comment and the following one with asterisks.
+ *****************************************************************************/
+
+
+/******************************************************************************
+ * Your code goes in between this comment and the preceeding one with asterisks
+ *****************************************************************************/
+
+    while (1);
+}
