eda4499cc0bc569631ed03b2a3fd5cf9d8c71de7
Julian To <jcto@ucsc.edu>
2018-06-04 14:16:26 -0700

    main switch statement

--- Lab09/Lab9.X/Agent.c
+++ Lab09/Lab9.X/Agent.c
@@ -1,11 +1,15 @@
 #include "Agent.h"
 #include "Protocol.h"
+#include "Field.h"
+#include "FieldOled.h"
+#include "Oled.h"
 
 NegotiationData nData;
 GuessData gData;
 
 ProtocolParserStatus myStatus;
-int agentEvent = AGENT_EVENT_NONE;
+int agentEvent = AGENT_EVENT_NONE; //flag for current AgentEvent
+static AgentState checkState = AGENT_EVENT_NONE;
 
 
 
@@ -14,28 +18,56 @@ int agentEvent = AGENT_EVENT_NONE;
 int AgentRun(char in, char *outBuffer)
 {
     myStatus = ProtocolDecode(in, &nData, &gData);
-    //check what ProtocolDecode returns
-    //then enter proper case and begin task
+    //check what ProtocolDecode returns and set a flag
+    if (in != NULL) {
+        if (checkState == PROTOCOL_PARSING_GOOD) {
+            agentEvent = AGENT_EVENT_NONE;
+        } else if (checkState == PROTOCOL_PARSED_CHA_MESSAGE) {
+            agentEvent = AGENT_EVENT_RECEIVED_CHA_MESSAGE;
+        } else if (checkState == PROTOCOL_PARSED_COO_MESSAGE) {
+            agentEvent = AGENT_EVENT_RECEIVED_COO_MESSAGE;
+        } else if (checkState == PROTOCOL_PARSED_DET_MESSAGE) {
+            agentEvent = AGENT_EVENT_RECEIVED_DET_MESSAGE;
+        } else if (checkState == PROTOCOL_PARSED_HIT_MESSAGE) {
+            agentEvent = AGENT_EVENT_RECEIVED_HIT_MESSAGE;
+        } else {
+            //if tne input isn't valid, set an error flag
+            agentEvent = AGENT_EVENT_MESSAGE_PARSING_FAILED;
+        }
+    }
     
-    switch (myStatus)
+    switch (checkState)
     {
-    case AGENT_EVENT_NONE:
+    case AGENT_STATE_GENERATE_NEG_DATA:
         
         break;
-    case AGENT_EVENT_MESSAGE_PARSING_FAILED:
+        
+    case AGENT_STATE_SEND_CHALLENGE_DATA:
         
         break;
-    case AGENT_EVENT_RECEIVED_COO_MESSAGE:
+        
+    case AGENT_STATE_DETERMINE_TURN_ORDER:
         
         break;
-    case AGENT_EVENT_RECEIVED_CHA_MESSAGE:
+        
+    case AGENT_STATE_SEND_GUESS:
         
         break;
-    case AGENT_EVENT_RECEIVED_HIT_MESSAGE:
+    case AGENT_STATE_WAIT_FOR_HIT:
         
         break;
-    case AGENT_EVENT_RECEIVED_DET_MESSAGE:
+    case AGENT_STATE_WAIT_FOR_GUESS:
         
         break;
-    }     
+    case AGENT_STATE_INVALID:
+        
+        break;
+    case AGENT_STATE_LOST:
+        
+        break;
+    case AGENT_STATE_WON:
+        
+        break;
+        
+    }
 }
