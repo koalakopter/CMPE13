d5ad3c1a503adb05e159193cf55455ea70bdd8d8
Julian To <jcto@ucsc.edu>
2018-06-04 23:16:02 -0700

    Wrote a pretty good test harness, will finish with partner

--- Lab09/ProtocolTest.c
+++ Lab09/ProtocolTest.c
@@ -24,12 +24,12 @@
 // **** Declare any function prototypes here ****
 GuessData gData;
 NegotiationData nData;
-NegotiationData xData; //for invalid ValidateNegotiationData
+NegotiationData xData; //for invalid ValidateNegotiationData and other testing
 ProtocolParserStatus pStatus;
+TurnOrder tOrder;
 
 char testMessage[PROTOCOL_MAX_PAYLOAD_LEN];
 int x;
-
 
 int main()
 {
@@ -37,12 +37,12 @@ int main()
 
 
 
-/******************************************************************************
- * Your code goes in between this comment and the following one with asterisks.
- *****************************************************************************/
+    /******************************************************************************
+     * Your code goes in between this comment and the following one with asterisks.
+     *****************************************************************************/
     printf("\nTest harness for CMPE 13 Lab 9 Protocol.c\n");
     printf("Testing Encode Message according to Lab Manual example outputs\n");
-            
+
     printf("\ntesting encode COO message\n");
     gData.row = 0;
     gData.col = 2;
@@ -50,46 +50,59 @@ int main()
     x = ProtocolEncodeCooMessage(testMessage, &gData);
     printf("Encoded string should have format: COO,%%u,%%u\n");
     printf("Encoded string: %sand Length of string: %d\n", testMessage, x);
-    
+
     printf("\ntesting encode HIT message\n");
     gData.hit = 0;
     x = ProtocolEncodeHitMessage(testMessage, &gData);
     printf("Encoded string should have format: HIT,%%u,%%u,%%u\n");
     printf("Encoded string: %sand Length of string: %d\n", testMessage, x);
-    
+
     printf("\ntesting encode CHA message\n");
     nData.hash = 139;
     nData.encryptedGuess = 54104;
     x = ProtocolEncodeChaMessage(testMessage, &nData);
     printf("Encoded string should have format: CHA,%%u,%%u,\n");
     printf("Encoded string: %sand Length of string: %d\n", testMessage, x);
-    
+
     printf("\ntesting encode DET message\n");
     nData.encryptionKey = 21382;
     nData.guess = 32990;
     x = ProtocolEncodeDetMessage(testMessage, &nData);
     printf("Encoded string should have format: DET,%%u,%%u,\n");
     printf("Encoded string: %sand Length of string: %d\n", testMessage, x);
-    
+
     //testing generate and validate negotiation Data
     printf("\nTesting Generate and Validate Negotiation Data\n");
     ProtocolGenerateNegotiationData(&nData);
     x = ProtocolValidateNegotiationData(&nData);
     printf("Generated and validated valid NegotiationData, expect a 1 here for TRUE: %d\n", x);
-    
+
     //generate invalid negotiationData (please ignore the errors in compilation)
     ProtocolGenerateNegotiationData(&xData);
     xData.hash = 12345; //changed the hash to some random number
     x = ProtocolValidateNegotiationData(&xData);
     printf("Generated an invalid NegotiationData hash key expect a 0 here for FALSE: %d\n", x);
+
+    printf("\nTesting Turn Order (-1: TIE, 0: DEFER, 1: START\n");
+    xData.encryptedGuess = 62132;
+    xData.encryptionKey = 16067;
+    xData.guess = 52343;
+    xData.hash = 70;
     
+    ProtocolGenerateNegotiationData(&nData);
+    ProtocolGenerateNegotiationData(&xData);
+    
+    tOrder = ProtocolGetTurnOrder(&nData, &xData);
+    printf("Turn Order: %d\n", tOrder);
+    tOrder = ProtocolGetTurnOrder(&xData, &nData);
+    printf("Turn Order (Switched): %d\n", tOrder);
     
     printf("\nTesting the Protocol Decode function\n");
     pStatus = ProtocolDecode(testMessage, &nData, &gData);
     printf("DECODE STATUS: %d (0 means PROTOCOL_WAITING)\n", pStatus);
-/******************************************************************************
- * Your code goes in between this comment and the preceeding one with asterisks
- *****************************************************************************/
+    /******************************************************************************
+     * Your code goes in between this comment and the preceeding one with asterisks
+     *****************************************************************************/
 
     while (1);
 }
