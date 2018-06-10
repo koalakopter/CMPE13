b4529be7e009d6fd66ef1eac20a9ca1965135186
Julian To <jcto@ucsc.edu>
2018-06-10 13:41:27 -0700

    GONNA COMMIT THESE ANYWAYS CAUSE IDK HOW TO USE UNIX

---
+++ Lab10/Lab10.X/Player.c
@@ -0,0 +1,41 @@
+#include "UNIXBOARD.h"
+#include "player.h"
+#include <stdint.h>
+#include <stdio.h>
+
+#define BOOLEAN int
+#define TRUE 1
+#define FALSE 0
+
+//inventory
+uint8_t inventory[INVENTORY_SIZE];
+static BOOLEAN added;
+static int loop;
+
+int AddToInventory(uint8_t item)
+{
+    added = FALSE;
+    loop = 0;
+    //loops through inventory until it finds an open slot, or inv is full
+    while (loop < INVENTORY_SIZE) {
+        if (inventory[loop] == NULL) {
+            //add the item
+            inventory[loop] = item;
+            return SUCCESS;
+        }
+        loop++;
+    }
+    return STANDARD_ERROR;
+}
+
+int FindInInventory(uint8_t item)
+{
+    loop = 0;
+    while (loop < INVENTORY_SIZE) {
+        if(inventory[loop] == item)
+        {
+            return SUCCESS;
+        }
+    }
+    return STANDARD_ERROR;
+}
